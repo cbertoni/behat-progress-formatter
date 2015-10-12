7f8c4d685ff8c2def652d67152cb6c35789ac0c6
NMserver <bertonicarlosalberto@gmail.com>
2015-10-12 16:12:57 -0300

    Changing apporach

--- src/BehatProgressFormatterExtension.php
+++ src/BehatProgressFormatterExtension.php
@@ -2,16 +2,14 @@
 
 namespace cbertoni\BehatProgressFormatter;
 
-use Behat\Testwork\ServiceContainer\Extension as ExtensionInterface;
+use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
-use Behat\Testwork\Exception\ServiceContainer\ExceptionExtension;
-use Behat\Testwork\Translator\ServiceContainer\TranslatorExtension;
 
-class BehatProgressFormatterExtension implements ExtensionInterface {
+class BehatProgressFormatterExtension implements Extension {
 
   public function process(ContainerBuilder $container) {
 
@@ -31,13 +29,10 @@ class BehatProgressFormatterExtension implements ExtensionInterface {
   }
 
   public function load(ContainerBuilder $container, array $config) {
-    $definition = new Definition('cbertoni\BehatProgressFormatter\ListPrinterFixed', array(
-        new Reference('output.node.printer.result_to_string'),
-        new Reference(ExceptionExtension::PRESENTER_ID),
-        new Reference(TranslatorExtension::TRANSLATOR_ID),
-        '%paths.base%'
+    $definition = new Definition('cbertoni\BehatProgressFormatter\ProgressStatisticsPrinterFixed', array(
+        new Reference('output.node.printer.counter'),
+        new Reference('output.node.printer.list')
     ));
-
-    $container->setDefinition('output.node.printer.list', $definition);
+    $container->setDefinition('output.node.printer.progress.statistics', $definition);
   }
 }
